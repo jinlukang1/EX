c85a5b3da9bc710f59b4f75851bb7f190e57405a
jinlukang1 <31444152+jinlukang1@users.noreply.github.com>
2018-09-20 17:02:12 +0800

    Add files via upload

--- copyFile.py
+++ copyFile.py
@@ -3,41 +3,41 @@
 import os,shutil
 import time
 
-airsimPath = "D:/Documents/AirSim/"
+airsimPath = "C:/Users/jlk06/Documents/AirSim/"
 # datetime = "2018-05-23-16-24-44"
 allFiles = os.listdir(airsimPath)
 allFiles.sort()
 # print allFiles[len(allFiles)-3]#有两个setting.json 所以是倒数第3个
-datetime = allFiles[len(allFiles)-3]
+datetime = allFiles[len(allFiles)-2]
 # time.sleep(100)
 realPath = airsimPath + datetime + "/images/"
-print realPath
+print(realPath)
 
-targetP = "D:/workspace/Unreal Projects/AirSim/PythonClient/temp/FFmpeg/bin/"
-targetD = targetP + "depth/"
+targetP = "D:/AirSim/"
+#targetD = targetP + "depth/"
 targetS = targetP + "segment/"
 targetR = targetP +"raw/"
 
 try:
-	shutil.rmtree(targetD)
+#	shutil.rmtree(targetD)
 	shutil.rmtree(targetS)
 	shutil.rmtree(targetR)
 except: 
-	print "Remove Error"
-os.mkdir(targetD)
+	print("Remove Error")
+#os.mkdir(targetD)
 os.mkdir(targetS)
 os.mkdir(targetR)
-print "successfully make dir"
+print("successfully make dir")
 
 
 alllist = os.listdir(realPath)
 for thisFile in alllist:
 	if "_0_0" in thisFile:
 		shutil.copyfile(realPath+thisFile,targetR+thisFile)
-	elif "_0_3" in thisFile:
-		shutil.copyfile(realPath+thisFile,targetD+thisFile)
+#	elif "_0_3" in thisFile:
+#		shutil.copyfile(realPath+thisFile,targetD+thisFile)
 	elif "_0_5" in thisFile:
 		shutil.copyfile(realPath+thisFile,targetS+thisFile)
 
-print "successfully moved"
+print("successfully moved")
 		
